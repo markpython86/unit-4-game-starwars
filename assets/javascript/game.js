8f9d7b5330b112ddd3e81b90a59fa3824d3b866e
Mark <mark.faraj86@gmail.com>
2018-10-26 19:19:56 -0500

    game done - need to add more visual effect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,55 +3,51 @@ console.log("we are live!!!")
 	// an array of objects(players)
 	var players = 	[rey = {
 						name: "Rey",
-						healthPoints: 80,
-						initialAttackPower: 6,
-						attackPower: 6,
-						counterAttackPower: 5,
+						healthPoints: 1080,
+						basicHitStrength: 15,
+						hitStrength: 12,
+						counterHitStrength: 12,
 						isPlayerOne: false,
 						isPlayerTwo: false,
-						isDead: false,
 					}, 
 					luke = {
 						name: "Luke Skywalker",
 						healthPoints: 150,
-						initialAttackPower: 5,
-						attackPower: 5,
-						counterAttackPower: 10,
+						basicHitStrength: 5,
+						hitStrength: 5,
+						counterHitStrength: 10,
 						isPlayerOne: false,
 						isPlayerTwo: false,
-						isDead: false,
 					},
 					maul = {
 						name: "Darth Maul",
 						healthPoints: 160,
-						initialAttackPower: 4,	
-						attackPower: 4,
-						counterAttackPower: 15,
+						basicHitStrength: 4,	
+						hitStrength: 4,
+						counterHitStrength: 15,
 						isPlayerOne: false,
 						isPlayerTwo: false,
-						isDead: false,
 					},
 					vader = {
 						name: "Darth Vader",
 						healthPoints: 180,
-						initialAttackPower: 2,
-						attackPower: 2,
-						counterAttackPower: 25,
+						basicHitStrength: 2,
+						hitStrength: 2,
+						counterHitStrength: 25,
 						isPlayerOne: false,
 						isPlayerTwo: false,
-						isDead: false,
-					}
-					]
+					}]
 	var player;
 	var enemy;
 	playerOneSelected = false;
 	playerTwoSelected = false;
 	gameStart = false;
 	var enemyHealthPoints;
-	var enemyCounterAttackPower;
-	var enemiesRemaining = players.length;
+	var enemycounterHitStrength;
+	var enemiesRemaining = players.length - 1;
 	console.log("enemies remaining is " + enemiesRemaining)
-	
+	playerSelect();
+	fight();
 
 
 
@@ -61,24 +57,22 @@ console.log("we are live!!!")
 		$("#rey").on("click", function() {
 			if (playerOneSelected === false){
 			console.log("rey is first player");
-			
 			$("#rey").appendTo("#player-fight-area");
 			$("#player-name").text(rey.name);
 			$("#rey").addClass('flip');
-			$("#player-hp").text("HealthPoints");
+			$("#player-hp").text("Health Points");
 			$("#player-total-hp").text(rey.healthPoints);
 			playerOneSelected = true;
 			rey.isPlayerOne = true;
 			player = rey;
-			enemySelect();
 			console.log("player one is " + playerOneSelected);
+			enemySelect();
 			}
 		});	
 	
 		$("#luke").on("click", function() {
 			if (playerOneSelected === false){
 				console.log("luke is first player");
-				
 				$("#luke").appendTo("#player-fight-area");
 				$("#luke").addClass('flip');
 				$("#player-name").text(luke.name);
@@ -95,9 +89,7 @@ console.log("we are live!!!")
 		$("#maul").on("click", function() {
 			if (playerOneSelected === false){
 				console.log("maul is first player");
-				
 				$("#maul").appendTo("#player-fight-area");
-
 				$("#player-name").text(maul.name);
 				$("#player-hp").text("Health Points");
 				$("#player-total-hp").text(maul.healthPoints);
@@ -113,7 +105,6 @@ console.log("we are live!!!")
 		$("#vader").on("click", function() {
 			if (playerOneSelected === false){
 				console.log("vader is first player");
-				
 				$("#vader").appendTo("#player-fight-area");
 				$("#vader").addClass('flip');
 				$("#player-name").text(vader.name);
@@ -140,16 +131,13 @@ console.log("we are live!!!")
 			isPlayerTwo = true;
 			gameStart = true;
 			enemy = rey;
-			console.log("player two is " + playerTwoSelected);
-			fight();
-			
+			console.log("player two is " + playerTwoSelected)			
 			}
 		});	
 	
 		$("#luke").on("click", function() {
 			if (playerTwoSelected === false && luke.isPlayerOne === false){
 				console.log("luke is second player");
-				
 				$("#luke").appendTo("#enemy-fight-area");
 				$("#enemy-name").text(luke.name);
 				$("#enemy-hp").text("Health Points");
@@ -159,9 +147,7 @@ console.log("we are live!!!")
 				gameStart = true;
 				enemy = luke;
 				console.log("gamestart is " + gameStart);
-				console.log("player two is " + playerTwoSelected);
-				fight();
-				
+				console.log("player two is " + playerTwoSelected);				
 			}
 		});	
 	
@@ -171,15 +157,14 @@ console.log("we are live!!!")
 				
 				$("#maul").appendTo("#enemy-fight-area");
 				$("#enemy-name").text(maul.name);
+				$("#maul").addClass('flip');
 				$("#enemy-hp").text("Health Points");
 				$("#enemy-total-hp").text(maul.healthPoints);
 				playerTwoSelected = true;
 				isPlayerTwo = true;
 				gameStart = true;
 				enemy = maul;
-				console.log("player two is " + playerTwoSelected);
-				fight();
-				
+				console.log("player two is " + playerTwoSelected);				
 			}
 		});	
 	
@@ -195,9 +180,7 @@ console.log("we are live!!!")
 				isPlayerTwo = true;
 				gameStart = true;
 				enemy = vader;
-				console.log("player two is " + playerTwoSelected);
-				fight();
-				
+				console.log("player two is " + playerTwoSelected);				
 			}
 		});	
 	}
@@ -205,13 +188,14 @@ console.log("we are live!!!")
 		$("#attackBtn").on("click", function() {
 			console.log("button pressed");
 			if (gameStart === true){
-				attack();  	
+				attack();
+				gainMorePower();  	
 				// console.log("enemy's HP is " + enemy.healthPoints);
 				if (enemy.healthPoints <= 0){
 				enemiesRemaining--;
 				console.log("enemies remaining is " + enemiesRemaining);
 				statusCheck();
-				}else {
+				} else {
 				counterAttack();
 					// console.log("rey's HP is " + player.healthPoints);
 				}
@@ -221,38 +205,70 @@ console.log("we are live!!!")
 		});		
 	}
 
+	
+
 	function attack(){
-		enemy.healthPoints = enemy.healthPoints - player.attackPower;
+		enemy.healthPoints = enemy.healthPoints - player.hitStrength;
 		$("#enemy-total-hp").text(enemy.healthPoints);
 		console.log("enemy HP is " + enemy.healthPoints);
 	}
 
 	function counterAttack(){
-		player.healthPoints = player.healthPoints - enemy.counterAttackPower;
+		player.healthPoints = player.healthPoints - enemy.counterHitStrength;
 		$("#player-total-hp").text(player.healthPoints);
-		console.log("player HP is " + player.healthPoints);
+		console.log("player Health is " + player.healthPoints);
+		console.log("Enemy counter hit strength is " + enemy.counterHitStrength)
 		if (player.healthPoints <= 0){
 			console.log("you lose");
-			statusCheck();	
+			// statusCheck();
+			gameLose();	
 		}
 	}
 
 	function statusCheck(){
-		if (enemiesRemaining === 0){
-			console.log("you win!");
+			if (enemiesRemaining === 0){
+			gameWin();
 		}
-		else if (player.healthPoints <= 0 || enemy.healthPoints <=0){
-
-			console.log(enemiesRemaining);
-			playerTwoSelected = false;
+		else {	
+			gameStart = false;
+				if (enemiesRemaining === 2){
+				} else if (enemiesRemaining === 1){
+		}
 			$("#enemy-fight-area").empty();
 			$("#enemy-name").text("");
 			$("#enemy-total-hp").text("");
-			$("#enemy-hp").text("");
-			enemySelect();
-		}
+			$('#enemy-hp').text("");
+			playerTwoSelected = false;
+			console.log("player two selected is " + playerTwoSelected);
+		}	
 	}
 
-playerSelect();
-	console.log("player one is " + playerOneSelected);
+
+
+	function gameWin() {
+		console.log("you win");
+		alert("you win!");
+		//player win screen
+		//player win music
+		$("#enemy-total-hp").text(0);
+		gameStart = false;	
+	}
+
+
+
+ 	function gameLose() {
+		console.log("you lose");
+		//player lose screen
+		//player lose music
+		$("#player-total-hp").text(0);
+		gameStart = false;
+	}
+
+	function gainMorePower(){
+		player.hitStrength = player.hitStrength + player.basicHitStrength;
+			console.log("player hit strength is " + player.hitStrength);
+	}	
+
+
+
 });
